1cf8c2b2cdbf31c8c44c5a274940dbf797b8020b
Pedro Esparza <isc.pedroesparza@gmail.com>
2023-08-07 10:57:40 -0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CasasRedOfficial'`, () => {
+  it(`should have as title 'casasredlandingpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CasasRedOfficial');
+    expect(app.title).toEqual('casasredlandingpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CasasRedOfficial app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('casasredlandingpage app is running!');
   });
 });
